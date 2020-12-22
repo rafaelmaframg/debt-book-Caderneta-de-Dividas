4b79467c77832aa06d7e361fa0ceec606a653994
Rafael Mafra <rafaelmafr4@hotmail.com>
2020-12-22 20:04:44 -0300

    adicionado relatorio detalhado por usuario, relatorio simplificado exibe data do ultimo acerto realizado

--- funcoes.py
+++ funcoes.py
@@ -14,8 +14,10 @@ def menu():
         print('\nSelecione a opção desejada:')
         print("[1] - Cadastrar Devedor")
         print("[2] - Listar Devedores")
-        print('[3] - Deletar dados cliente')
-        print("[4] - Sair do Programa")
+        print("[3] - Exibir Relatório de pagamentos")
+        print('[4] - Deletar dados cliente')
+        print("[5] - Sair do Programa")
+    
         try:
             op = int(input())
         except:
@@ -26,38 +28,22 @@ def menu():
             cadastro()
         elif op == 2:
             leitura()
-        elif op == 4:
-            arquivo.save('devedores.xlsx')
-            exit()
         elif op == 3:
+            relatorio()
+        elif op == 4:
             excluiusuario()
             arquivo.save('devedores.xlsx')
+        elif op == 5:
+            arquivo.save('devedores.xlsx')
+            exit()
+        
+        
         else:
             print("Informe um valor correto 1,2,3 ou 4")
             menu()
-def leitura(): 
-    excluizero()
-    max_linha= planilha1.max_row
-    for i in range (1, max_linha+1):
-        if not planilha1.cell(row=i, column=1).value == None:    
-            print(planilha1.cell(row=i, column=1).value, end=" - ")
-            print(planilha1.cell(row=i, column=2).value, end=" ")
-            print(planilha1.cell(row=i, column=3).value, end=" ")
-            print("Ultima Atualização:", end=' ')
-            print(planilha1.cell(row=i, column=4).value, end="\n")           
-            print(linha)
-
-def verifica_registro():
-    consultados = []
-    max_linha= planilha1.max_row
-    for i in range(1,max_linha+1):
-        consultado = planilha1.cell(row=i, column=1).value        
-        consultados.append(consultado)
-    return consultados
 
 def cadastro():
     devedores = ()
-    moeda = "R$"
     dataatual = date.today()
     data = dataatual.strftime('%d/%m/%Y')
     try: 
@@ -68,10 +54,14 @@ def cadastro():
         if devedor not in consultados:
             divida = str(input('Digite o Valor da Divida: R$'))
             divida = divida.replace(",",".")
-            devedores = devedor,moeda, divida,data
+            if divida < "0":
+                print("Insira apenas valores positivos\nOperação cancelada!")
+                menu()
+            devedores = divida,devedor,data,divida
             planilha1.append(devedores)
             arquivo.save('devedores.xlsx')
         else:
+            max_coluna = planilha1.max_column
             print(linha)
             print("Usuario já cadastrado")
             print(linha)
@@ -80,25 +70,34 @@ def cadastro():
             if fin == "1":
                 somar = str(input("Digite o valor da divida a SOMAR [+]:  "))
                 somar = somar.replace(",",".")  
+                if somar < "0":
+                    print("Insira apenas valores positivos\nOperação cancelada!")
+                    menu()
                 pos = consultados.index(devedor)
-                valor = float(planilha1['C'+str(pos+1)].value)+float(somar)
-                planilha1['C'+str(pos+1)] = str(valor)
-                planilha1['D'+str(pos+1)] = str(data)
+                valor = float(planilha1['A'+str(pos+1)].value)+float(somar)
+                planilha1['A'+str(pos+1)] = str(valor)
+                for i in range (2, max_coluna+2):
+                    if planilha1.cell(row=pos+1, column=i).value == None:
+                        planilha1.cell(row=pos+1, column=i).value = str(data)
+                        planilha1.cell(row=pos+1, column=i+1).value = str(somar)
                 print(linha)
-                print("Novo Valor de R${} atualizado para {}".format(planilha1['C'+str(pos+1)].value, devedor))
+                print("Novo Valor de R${} atualizado para {}".format(planilha1['A'+str(pos+1)].value, devedor))
             elif fin == "2":
                 abate = str(input("Digite o valor a ser ABATIDO [-]:  "))
                 abate = abate.replace(",",".")
                 pos = consultados.index(devedor)
-                valor = float(planilha1['C'+str(pos+1)].value)-float(abate)
-                planilha1['C'+str(pos+1)] = str(valor)
-                planilha1['D'+str(pos+1)] = str(data)
+                valor = float(planilha1['A'+str(pos+1)].value)-float(abate)
+                planilha1['A'+str(pos+1)] = str(valor)
+                for i in range(2, max_coluna+2):
+                    if planilha1.cell(row=pos+1, column=i).value == None:
+                        planilha1.cell(row=pos+1, column=i).value = str(data)
+                        planilha1.cell(row=pos+1, column=i+1).value = "-"+str(abate)
                 print(linha)
-                print("Novo Valor de R${} atualizado para {}".format(planilha1['C'+str(pos+1)].value, devedor))
+                print("Novo Valor de R${} atualizado para {}".format(planilha1['A'+str(pos+1)].value, devedor))
             else:
                 print("Digite um valor valido, 1 ou 2")
                 menu()
-        excluizero()
+        arquivo.save('devedores.xlsx')     
         print(linha)
         
     except:
@@ -106,17 +105,54 @@ def cadastro():
         print("Formato de entrada invalido")
         print(linha)
         menu()
-    
-def excluizero():
-    for i in range(1, planilha1.max_row+1):
-        if planilha1.cell(row=i, column=3).value == "0" or planilha1.cell(row=i, column=3).value == "0.0":
-            planilha1.delete_rows(i)
-    for i in range(1, planilha1.max_row+1):
-        if planilha1.cell(row=i, column=1).value == None:
-            planilha1.delete_rows(i)
 
-def excluiusuario():
-   
+def leitura():
+    max_linha= planilha1.max_row
+    max_coluna= planilha1.max_column
+    for i in range (1, max_linha+1):
+        if planilha1.cell(row=i, column=1).value == "0.0" or planilha1.cell(row=i, column=1).value == None:
+            continue
+          
+        print(planilha1.cell(row=i, column=2).value, end="\n")
+        print("data:", end=' ')
+        for j in range(2, max_coluna+2):
+            if planilha1.cell(row=i, column=j).value == None and planilha1.cell(row=i, column=j-1).value != None:
+                print((planilha1.cell(row=i, column=j-2).value), end=" ")
+        print("R$:", end='')
+        print(planilha1.cell(row=i, column=1).value, end="\n")                  
+        print(linha)
+
+def relatorio():
+    max_coluna = planilha1.max_column
+    try:
+        devedor = str(input('Digite o nome do cliente a ser pesquisado:')).lower()
+        print(linha)
+        devedor = devedor.capitalize()
+        registro = verifica_registro()
+        pos = registro.index(devedor)
+        print("Exibindo resultados para {}".format(devedor))
+        for j in range(3, max_coluna+1):
+            if j%2==0:
+                print("R$",end="")
+                print(planilha1.cell(row=pos+1, column=j).value, end="\n")
+            else:
+                print(planilha1.cell(row=pos+1, column=j).value, end=" ")
+        print("Resultado Geral: {}".format(planilha1.cell(row=pos+1, column=1).value))   
+        print(linha)
+    except: 
+        print("Usuario invalido")
+        menu()
+    
+    
+def verifica_registro():
+    consultados = []
+    max_linha= planilha1.max_row
+    for i in range(1,max_linha+1):
+        consultado = planilha1.cell(row=i, column=2).value        
+        consultados.append(consultado)
+    return consultados
+
+def excluiusuario(): 
     try: 
         consultados = verifica_registro()
         devedor = str(input("Insira o nome do devedor a ser removido: ")).lower()
--- principal.py
+++ principal.py
@@ -13,3 +13,10 @@ menu()
     #verificar dividas dos devedores
     #realizar aporte dos valores devidos com datas
     #emitir relatorios sobre dividas
+
+
+
+    #ideia caloteira
+    #adicionar função para visualisar debitos e creditos por usuario
+    #-listar os nomes -> listar debitos por usuario
+    
